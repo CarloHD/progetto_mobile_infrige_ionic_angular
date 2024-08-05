b478953b78ba071d43d6daaad070dc8e39415bf4
CarloHD <carloborrometi@gmail.com>
2024-08-05 18:30:14 +0200

    [fix] miglioramento routing e layout

--- src/app/layout/pages/full/full-routing.module.ts
+++ src/app/layout/pages/full/full-routing.module.ts
@@ -1,30 +1,23 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
-import { FullPage } from './full.page';
-
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 const routes: Routes = [
   {
+    path: 'home',
+    loadChildren: () => import('../../../pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'settings',
+    loadChildren: () => import('../../../pages/settings/settings.module').then(m => m.SettingsPageModule)
+  },
+  {
     path: '',
-    component: FullPage,
-    children: [ {
-      path: 'home',
-      loadChildren: () => import( '../../../pages/home/home.module' ).then( m => m.HomePageModule )
-    }, {
-      path: 'settings',
-      loadChildren: () => import( '../../../pages/settings/settings.module' ).then( m => m.SettingsPageModule )
-    }, {
-      path: '',
-      redirectTo: 'home',
-      pathMatch: 'full'
-    } ]
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }];
 
-  }
-];
-
-@NgModule( {
-  imports: [ RouterModule.forChild( routes ) ],
-  exports: [ RouterModule ],
-} )
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
 export class FullPageRoutingModule {}
--- src/app/layout/pages/full/full.page.ts
+++ src/app/layout/pages/full/full.page.ts
@@ -2,12 +2,18 @@ import { Component, OnInit } from '@angular/core';
 
 import { ModalService } from 'src/app/services/modal/modal.service';
 import { ChangePageService } from 'src/app/services/change-page/change-page.service';
+import { SidenavComponent } from '../../components/sidenav/sidenav.component';
+import { IonicModule } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { FullPageRoutingModule } from './full-routing.module';
 
 
 @Component({
   selector: 'app-full',
   templateUrl: './full.page.html',
   styleUrls: ['./full.page.scss'],
+  standalone: true,
+  imports: [IonicModule, FullPageRoutingModule, CommonModule, SidenavComponent]
 
 })
 export class FullPage implements OnInit {
